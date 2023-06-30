d13af38a52a6d70a3de800c44dcd7c3977cc5b30
pedrobadm7 <pedrobars7a@gmail.com>
2023-06-30 07:41:40 -0300

    feature: Improve dashboard styles

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,22 @@
 import * as S from './styles'
 
+
 export function Dashboard(){
   return (
    <S.Container>
-    <S.Title>Dashboard</S.Title>
+    <S.Header>
+      <S.UserWrapper>
+      <S.UserInfo>
+        <S.Photo source={{uri:'https://avatars.githubusercontent.com/u/78272705?v=4' }}/>
+        <S.User>
+          <S.UserGreeting>Olá</S.UserGreeting>
+          <S.UserName>Pedro</S.UserName>
+        </S.User>
+      </S.UserInfo>
+
+      <S.Icon name='power'/>
+      </S.UserWrapper>   
+    </S.Header>
   </S.Container>  
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,58 @@
 import styled, { DefaultTheme } from 'styled-components/native'
+import { RFPercentage, RFValue} from 'react-native-responsive-fontsize'
+import {Feather} from '@expo/vector-icons'
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }: DefaultTheme) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({theme }: DefaultTheme) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({theme}: DefaultTheme) => theme.colors.title };
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+
+  background-color: ${({theme}: DefaultTheme) => theme.colors.primary };
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({theme}: DefaultTheme) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}: DefaultTheme) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({theme}: DefaultTheme) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}: DefaultTheme) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({theme}: DefaultTheme) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`
